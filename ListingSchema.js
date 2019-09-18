89c368f7104a0d03d1771f8bea2f8f20945066c7
Jennifer Bernal <jennifer.bernal@ufl.edu>
2019-09-17 20:47:12 -0400

    added finished ListingSchemas

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,14 +1,28 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose') ,
+Schema = mongoose.Schema;
+
+
 
 /* Create your schema for the data in the listings.json file that will define how data is saved in your database
      See https://mongoosejs.com/docs/guide.html for examples for creating schemas
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-  */
+ */
 var listingSchema = new Schema({
   /* Your code for a schema here */ 
   //Check out - https://mongoosejs.com/docs/guide.html
+
+  //info possible in a Listing
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true, unique: true},
+  coordinates: { 
+    latitude: Number,
+    longitude: Number
+  },
+  address: String,
+  
+  created_at: Date,
+  updated_at: Date
 
 });
 
@@ -16,11 +30,22 @@ var listingSchema = new Schema({
    See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  //retreive current date
+  var date = new Date();
+  this.updated_at = date;
+  if(!this.created_at) // just created, no date added yet
+    this.created_at = date;
+  
+    
+  next();
 });
 
-/* Use your schema to instantiate a Mongoose model */
-//Check out - https://mongoosejs.com/docs/guide.html#models
+/* Check out - https://mongoosejs.com/docs/guide.html#models
+  Use your schema to instantiate a Mongoose model 
+  Make a "document" (model instance)
+  mongoose.model uses default connection (else replace mongoose w/ connection)
+  documentName = *.(modelName, schema)
+*/
 var Listing = mongoose.model('Listing', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
